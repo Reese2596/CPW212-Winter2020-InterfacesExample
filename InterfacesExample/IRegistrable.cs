30d97792ed8720fab77a80b0fb70a6bcdd95a904
Makayla Reese <makaylareese15@gmail.com>
2020-03-08 17:11:57 -0700

    Add project files.

---
+++ InterfacesExample/IRegistrable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample
+{
+    interface IRegistrable
+    {
+        void Register(IEnumerable<String> courseCodes);
+    }
+
+    [DebuggerDisplay("{FullName,nq} {GradePointAverage}")]
+    public class Student : IRegistrable, IComparable<Student>
+    {
+        public string FullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public double GradePointAverage { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return GradePointAverage.CompareTo(other.GradePointAverage);
+            //if (this.GradePointAverage < other.GradePointAverage)
+            //{
+            //    return -1;
+            //}
+            //else if (GradePointAverage == other.GradePointAverage)
+            //{
+            //    return 0;
+            //}
+            //else
+            //{
+            //    return 1;
+            //}
+        }
+
+        public void Register(IEnumerable<string> codes)
+        {
+            foreach (string code in codes)
+            {
+                Console.WriteLine("Registered for " + code);
+            }
+        }
+    }
+
+    public class StudentAgeCompare : IComparer<Student>
+    {
+        public int Compare(Student x, Student y) => x.DateOfBirth.CompareTo(y.DateOfBirth);
+    }
+}
---
+++ InterfacesExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student
+            {
+                FullName = "Jane Doe",
+                DateOfBirth = DateTime.Today,
+                GradePointAverage = 3.5
+            };
+
+            Student s2 = new Student
+            {
+                FullName = "John Doe",
+                DateOfBirth = DateTime.Today,
+                GradePointAverage = 4
+            };
+
+            List<Student> roster = new List<Student>()
+            {
+                s2, s
+            };
+            roster.Sort(new StudentAgeCompare());
+
+            roster = (from stu in roster
+                      orderby stu.DateOfBirth descending
+                      select stu).ToList();
+
+            var courses = new List<string>
+            {
+                "CPW 101",
+                "CPW 150",
+                "CPW 118"
+            };
+
+            RegisterPErson(s, courses);
+            Console.ReadKey();
+        }
+
+        private static void RegisterPErson(IRegistrable registrable, List<string> courses)
+        {
+            registrable.Register(courses);
+        }
+    }
+}
